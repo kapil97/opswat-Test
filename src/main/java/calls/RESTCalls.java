8a91d0ca663a281fa5151316f18623687a6bd28c
kapil97 <kragava1@binghamton.edu>
2021-05-02 04:04:20 -0400

    major refactoring done

--- src/main/java/calls/RESTCalls.java
+++ src/main/java/calls/RESTCalls.java
@@ -3,25 +3,18 @@ package calls;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import utils.FileProcessor;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class RESTCalls implements RestCallsI{
     private String dataID;
-    private String hash;
-    private final String testUrl = "https://jsonplaceholder.typicode.com/albums";
-    private final String hashUrl = "https://api.metadefender.com/v4/hash/";
+    private final String retrieveFileResultsUrl = "https://api.metadefender.com/v4/file/";
     private final String uploadFileUrl = "https://api.metadefender.com/v4/file";
-    private final String retriveFileResultsUrl = "https://api.metadefender.com/v4/file/";
+    private final String hashUrl = "https://api.metadefender.com/v4/hash/";
     private final HttpClient httpClient;
     private String filepath;
     private String apikey;
@@ -49,37 +42,34 @@ public class RESTCalls implements RestCallsI{
                     .uri(URI.create(uploadFileUrl))
                     .build();
             response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
-//            System.out.println(response.body());
 
         } catch (InterruptedException | IOException e) {
             System.err.println("No such file found");
             e.printStackTrace();
         }
+
         JSONObject jsonObject = new JSONObject(response.body());
-        JSONObject errorObject = null;
-        boolean successful = false;
-        try {
-            errorObject = jsonObject.getJSONObject("error");
-            if(errorObject!=null){
-                int code = errorObject.getInt("code");
-                if(code!= 400140) System.err.println("No endpoint found");
-                successful = !(code == 400140);
-                System.err.println(errorObject.getJSONArray("messages").toString());
-            }
+
+        if(jsonObject.has("error")) {
+            JSONObject errorObject = jsonObject.getJSONObject("error");
+            int code = errorObject.getInt("code");
+            if(code!= 400140) System.err.println("No endpoint found");
+            System.err.println(errorObject.getJSONArray("messages").toString());
+            return false;
         }
-        catch (JSONException exception){
+        else {
             this.dataID = jsonObject.getString("data_id");
-            successful = jsonObject.getString("status").equals("inqueue");
             System.out.println("dataID: " +dataID);
+            return jsonObject.getString("status").equals("inqueue");
         }
-        return successful;
     }
 
     public boolean ifHashExists(String hash){
+
         HttpRequest request = HttpRequest.newBuilder().GET()
                 .header("apikey",apikey)
                 .header("content-type","application/JSON")
-                .uri(URI.create(hashUrl+hash))
+                .uri(URI.create(hashUrl +hash))
                 .build();
         HttpResponse<String> response = null;
         try {
@@ -90,35 +80,49 @@ public class RESTCalls implements RestCallsI{
 
 
         JSONObject jsonObject = new JSONObject(response.body());
-        boolean result = false;
-        JSONObject errorObject  = null;
-
-        try {
-            errorObject = jsonObject.getJSONObject("error");
-//            System.out.println("Successful: ");
-//            System.out.println(jsonObject);
-            if (errorObject != null) {
-//                System.out.println("Error: " + errorObject.getInt("code") + " messages: " + errorObject.getJSONArray("messages"));
+        if(jsonObject.has("error")){
+                JSONObject errorObject = jsonObject.getJSONObject("error");
                 int code = errorObject.getInt("code");
                 if(code!= 404003) System.err.println("No endpoint found");
-                result = !(code == 404003);
                 System.err.println(errorObject.getJSONArray("messages").toString());
-            }
+                return false;
         }
-        catch (JSONException exception){
+        else {
             this.dataID = jsonObject.getString("data_id");
-//            System.out.println("dataID: " +dataID);
-            result = true;
+            System.out.println("dataID: " +dataID);
+            return true;
         }
-        return result;
     }
 
     public boolean retrieveScanResults(){
+        HttpRequest request = HttpRequest.newBuilder()
+                .header("apikey",apikey)
+                .uri(URI.create(retrieveFileResultsUrl+dataID))
+                .build();
+        HttpResponse<String> response = null;
+        try{
+            response = httpClient.send(request,HttpResponse.BodyHandlers.ofString());
+        }
+        catch ( IOException | InterruptedException exception){
+            exception.printStackTrace();
+        }
+        JSONObject jsonObject = new JSONObject(response.body());
+        if(jsonObject.has("status") && jsonObject.getString("status").equals("inqueue")){
+
+        }
+        else if(jsonObject.has("error")){
+
+        }
+        else{
+
+        }
         return false;
     }
 
 
     public void testJsonGETReq(){
+        //    private String hash;
+        String testUrl = "https://jsonplaceholder.typicode.com/albums";
         HttpRequest request = HttpRequest.newBuilder().GET()
                 .header("content-type","application/JSON")
                 .uri(URI.create(testUrl))
@@ -129,7 +133,7 @@ public class RESTCalls implements RestCallsI{
         } catch (IOException | InterruptedException exception) {
             exception.printStackTrace();
         }
-        System.out.println(response.body());
+//        System.out.println(response.body());
 
         JSONArray jsonArray = new JSONArray(response.body());
         for(int i=0; i<jsonArray.length(); i++){
